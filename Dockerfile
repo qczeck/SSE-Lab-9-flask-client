61efd94a41eac84d982b5f45f4a3c94f33a2b843
qczeck <maciej.kuczek@icloud.com>
2025-01-29 11:17:56 +0000

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-FROM python:3.9
+FROM python:3.9-slim
 
 WORKDIR /app
 
 COPY requirements.txt .
-RUN pip install -r requirements.txt
+RUN pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
 EXPOSE 5000
 
-CMD ["python" "app.py"]
+CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request, render_template, jsonify
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello from William and Maciej!"
+BOOKS_SERVICE_URL = "http://sse-lab10-simple.gucqgnd7b7azbuax.uksouth.azurecontainer.io:5000/books"
+
+@app.route('/', methods=['GET', 'POST'])
+def search_books():
+    if request.method == 'POST':
+        genre = request.form.get('genre')
+        response = requests.get(BOOKS_SERVICE_URL)
+        if response.status_code == 200:
+            books = response.json()
+            if genre:
+                filtered_books = [book for book in books if book['genre'].lower() == genre.lower()]
+            else:
+                filtered_books = books
+            return render_template('results.html', books=filtered_books)
+    return render_template('search.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
