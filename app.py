c9d4fde88e04fcc95a6ffdfdcb2465460e429187
qczeck <maciej.kuczek@icloud.com>
2025-01-31 11:40:58 +0000

    Extension

--- app.py
+++ app.py
@@ -9,14 +9,10 @@ BOOKS_SERVICE_URL = "http://sse-lab10-simple.gucqgnd7b7azbuax.uksouth.azureconta
 def search_books():
     if request.method == 'POST':
         genre = request.form.get('genre')
-        response = requests.get(BOOKS_SERVICE_URL)
+        response = requests.get(BOOKS_SERVICE_URL, params={'genre': genre})
         if response.status_code == 200:
             books = response.json()
-            if genre:
-                filtered_books = [book for book in books if book['genre'].lower() == genre.lower()]
-            else:
-                filtered_books = books
-            return render_template('results.html', books=filtered_books)
+            return render_template('results.html', books=books)
     return render_template('search.html')
 
 if __name__ == '__main__':
